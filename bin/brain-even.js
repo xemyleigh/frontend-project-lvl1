d230fe249435b0bc34e5ae5d4c635cc1323e58a4
alexander <emyleigh@inbox.ru>
2022-03-20 11:52:51 +0300

    type fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,7 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const gameData = () => {
   const number = Math.floor(Math.random() * 100);
-  const question = number.toString();
+  const question = number;
   const correctAnswer = (question % 2 === 0) ? 'yes' : 'no';
   return { question, correctAnswer };
 };
